845199e04059b2a5315471281bc9f3a03bc57a7b
Jason R. Jones <slightlynybbled@gmail.com>
2016-07-13 10:45:47 -0400

    Moved mock serial port to its own file

--- test_frame.py
+++ test_frame.py
@@ -1,30 +1,7 @@
 from serialdispatch.dispatchframe import Frame
-import serial
+from test_mockserial import MockSerialPort
 import pytest
 import time
-
-
-class MockSerialPort(serial.Serial):
-    """ This is a class that will provide a mock interface for the serial port """
-
-    def __init__(self):
-        self.serial_data_out = []
-        self.serial_data_in = []
-
-    def close(self):
-        del self.serial_data_out
-
-    def flushInput(self):
-        self.serial_data_in = []
-
-    def write(self, byte_array):
-        for e in byte_array:
-            self.serial_data_out.append(e)
-
-    def read(self, num_of_bytes):
-        data_to_return = self.serial_data_in[:num_of_bytes]
-        self.serial_data_in = self.serial_data_in[num_of_bytes:]
-        return data_to_return
 
 
 def test_push_tx_message():
---
+++ test_mockserial.py
@@ -0,0 +1,23 @@
+import serial
+
+class MockSerialPort(serial.Serial):
+    """ This is a class that will provide a mock interface for the serial port """
+
+    def __init__(self):
+        self.serial_data_out = []
+        self.serial_data_in = []
+
+    def close(self):
+        del self.serial_data_out
+
+    def flushInput(self):
+        self.serial_data_in = []
+
+    def write(self, byte_array):
+        for e in byte_array:
+            self.serial_data_out.append(e)
+
+    def read(self, num_of_bytes):
+        data_to_return = self.serial_data_in[:num_of_bytes]
+        self.serial_data_in = self.serial_data_in[num_of_bytes:]
+        return data_to_return
