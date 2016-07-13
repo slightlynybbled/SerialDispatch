6fe15941283d145dcd6853dcda29fbfbf579a473
Jason R. Jones <slightlynybbled@gmail.com>
2016-07-13 09:51:48 -0400

    Added frame reception test

--- test_frame.py
+++ test_frame.py
@@ -70,6 +70,22 @@ def test_rx_is_available():
     port = MockSerialPort()
     frame = Frame(port)
 
+    # this is the serial representation of framed data, with checksum
+    port.serial_data_in = [frame.SOF, 1, 2, frame.ESC, frame.SOF ^ frame.ESC_XOR,
+                           4, frame.ESC, frame.EOF ^ frame.ESC_XOR,
+                           6, frame.ESC, frame.ESC ^ frame.ESC_XOR,
+                           8, 9, 10, 148, 20, frame.EOF]
+    
+    
+    time.sleep(0.1)
+
+    assert frame.rx_is_available() is True
+    
+def test_pull_rx_message():
+    port = MockSerialPort()
+    frame = Frame(port)
+    
+    # this is the serial representation of framed data, with checksum
     port.serial_data_in = [frame.SOF, 1, 2, frame.ESC, frame.SOF ^ frame.ESC_XOR,
                            4, frame.ESC, frame.EOF ^ frame.ESC_XOR,
                            6, frame.ESC, frame.ESC ^ frame.ESC_XOR,
@@ -78,8 +94,9 @@ def test_rx_is_available():
     
     time.sleep(0.1)
     
-    #print(frame.rx_messages)
+    # this is the expected 'de-framed' data
+    data_to_receive = [1, 2, frame.SOF, 4, frame.EOF, 6, frame.ESC, 8, 9, 10]
 
-    assert frame.rx_is_available() is True
+    assert frame.pull_rx_message() == data_to_receive
 
 
