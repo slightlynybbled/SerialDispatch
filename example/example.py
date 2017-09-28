6b39e2e6f16e914a8494cafce4fe64e45eb43dc1
Jason R. Jones <slightlynybbled@gmail.com>
2017-09-28 14:43:53 -0400

    Fixed spacing, added some time for processor to deal with other tasks

--- example/example.py
+++ example/example.py
@@ -1,11 +1,13 @@
 import serial
 import serialdispatch
+import time
 
 # the timeout keeps the serial port polling appropriately
 port = serial.Serial('COM9', 57600, timeout=0.1)
 
 # a serial port must be supplied to SerialDispatch
 sd = serialdispatch.SerialDispatch(port)
+
 
 def subscriber():
     # the data is retrieved by topic
@@ -15,4 +17,4 @@ def subscriber():
 sd.subscribe('subscriber topic', subscriber)
 
 while True:
-    pass
+    time.sleep(0.001)
