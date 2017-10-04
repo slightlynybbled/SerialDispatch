891d1f91dbd163f4e0ab1c17d35bcfd25764ac8f
Jason R. Jones <slightlynybbled@gmail.com>
2017-10-04 08:05:08 -0400

    Elaborating on the example

--- example/example.py
+++ example/example.py
@@ -1,9 +1,14 @@
 import serial
 import serialdispatch
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 # the timeout keeps the serial port polling appropriately
-port = serial.Serial('COM9', 57600, timeout=0.1)
+port = serial.Serial('COM18', 57600, timeout=0.1)
 
 # a serial port must be supplied to SerialDispatch
 sd = serialdispatch.SerialDispatch(port)
@@ -11,10 +16,11 @@ sd = serialdispatch.SerialDispatch(port)
 
 def subscriber():
     # the data is retrieved by topic
-    data = sd.get('subscriber topic')
+    data = sd.get('i')
     print('message received: ', data)
     
-sd.subscribe('subscriber topic', subscriber)
+sd.subscribe('i', subscriber)
 
 while True:
-    time.sleep(0.001)
+    sd.publish('foo', [0], ['U16'])
+    time.sleep(1.0)
---
+++ example/readme.md
@@ -0,0 +1,3 @@
+# Purpose
+
+This file is intended to work with the [example dispatch file](https://github.com/slightlynybbled/Dispatch/tree/master/examples).
